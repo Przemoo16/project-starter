82797ae4d2cf8ae7fb1bf1597d2ce324bc15e4af
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-02-04 00:06:19 +0100

    Set up JWT

--- backend/app/config/general.py
+++ backend/app/config/general.py
@@ -1,3 +1,4 @@
+import datetime
 import functools
 import logging
 from typing import Any
@@ -10,6 +11,16 @@ class Settings(pydantic.BaseSettings):
     API_VERSION: str = "/api/v1"
 
     # Security
+    SECRET_KEY: str
+    AUTHJWT_SECRET_KEY: str
+    AUTHJWT_ACCESS_TOKEN_EXPIRES: datetime.timedelta = datetime.timedelta(minutes=30)
+    AUTHJWT_REFRESH_TOKEN_EXPIRES: datetime.timedelta = datetime.timedelta(days=1)
+    AUTHJWT_DENYLIST_ENABLED: bool = True
+    AUTHJWT_DENYLIST_TOKEN_CHECKS: set[str] = {"access", "refresh"}
+    AUTHJWT_TOKEN_LOCATION: set[str] = {"headers"}
+    AUTHJWT_ALGORITHM: str = "HS256"
+    AUTHJWT_DECODE_ALGORITHMS: set[str] = {"HS256"}
+    AUTHJWT_DATABASE_URL: pydantic.RedisDsn
     ACCOUNT_ACTIVATION_DAYS = 7
 
     # Database
@@ -41,6 +52,9 @@ class Settings(pydantic.BaseSettings):
             "level": logging.DEBUG,
         },
     }
+
+    class Config:
+        case_sensitive = True
 
 
 @functools.lru_cache()
