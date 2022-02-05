6f2efc27a41aa4f53a60d691dae7024f0197f734
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-02-05 16:00:31 +0100

    Delete routers

--- backend/app/api/v1/api.py
+++
@@ -1,8 +0,0 @@
-import fastapi
-
-from app.api.v1.routers import me, users
-
-router = fastapi.APIRouter()
-
-router.include_router(me.router, prefix="/me", tags=["me"])
-router.include_router(users.router, prefix="/users", tags=["users"])
