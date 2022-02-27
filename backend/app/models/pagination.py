8ed025d172b6eec470c972d6b0652ef5e7dfcd51
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-02-27 14:50:53 +0100

    Add pagination

---
+++ backend/app/models/pagination.py
@@ -0,0 +1,19 @@
+import typing
+
+import pydantic
+
+from app.models import base
+
+TotalResults: typing.TypeAlias = int
+
+PaginationOffset: typing.TypeAlias = pydantic.conint(ge=0)
+PaginationLimit: typing.TypeAlias = pydantic.PositiveInt | None
+
+
+class Pagination(base.BaseModel):
+    offset: PaginationOffset = 0
+    limit: PaginationLimit = None
+
+
+class PaginationResponse(base.BaseModel):
+    total: TotalResults
