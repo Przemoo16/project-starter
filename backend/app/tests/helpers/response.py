0e4479dd86ecc8aa8d6bbf569802e81252f6e789
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-03-04 08:28:01 +0100

    Serialize decimals in tests

--- backend/app/tests/helpers/response.py
+++ backend/app/tests/helpers/response.py
@@ -1,3 +1,4 @@
+import decimal
 import typing
 
 import orjson
@@ -5,5 +6,11 @@ import orjson
 from app.utils import converters
 
 
+def _orjson_default(obj: typing.Any) -> typing.Any:
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    raise TypeError
+
+
 def format_response(response: typing.Any) -> typing.Any:
-    return orjson.loads(converters.orjson_dumps(response))
+    return orjson.loads(converters.orjson_dumps(response, default=_orjson_default))
