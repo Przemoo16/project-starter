2cd54c1fb12ae517af1241433ecb94d8dedc5794
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-04-13 18:18:59 +0200

    Extract common components in layouts

--- frontend/src/ui-components/layouts/AnonymousLayout.tsx
+++ frontend/src/ui-components/layouts/AnonymousLayout.tsx
@@ -1,84 +1,36 @@
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { ReactNode } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link as RouterLink } from 'react-router-dom';
 
-import heroIcon from '../../assets/icons/hero.svg';
-import { Copyright } from '../Copyright';
-import { Logo } from '../Icons';
+import { ContainerWithHero } from './common/Container';
 
-interface Props {
+interface AnonymousLayoutProps {
   children: ReactNode;
 }
 
-export const AnonymousLayout = ({ children }: Props) => {
+export const AnonymousLayout = ({ children }: AnonymousLayoutProps) => {
   const { t } = useTranslation();
   return (
-    <Grid container component="main" sx={{ minHeight: '100vh' }}>
-      <Grid
-        item
-        xs={0}
-        md={6}
+    <ContainerWithHero>
+      <Box sx={{ width: '100%', mt: 10, mb: 5 }}>{children}</Box>
+      <Box
         sx={{
-          display: { xs: 'none', md: 'block' },
-          backgroundImage: `url(${heroIcon})`,
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: '80% 80%',
-          backgroundPosition: 'center',
-        }}
-      />
-      <Grid
-        item
-        component={Paper}
-        elevation={6}
-        square
-        xs={12}
-        md={6}
-        sx={{
-          px: 5,
+          width: '100%',
+          maxWidth: 500,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
         }}
       >
-        <Box
-          component="section"
-          sx={{
-            width: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexGrow: 1,
-          }}
-        >
-          <Logo />
-          <Box sx={{ width: '100%', mt: 10, mb: 5 }}>{children}</Box>
-          <Box
-            sx={{
-              width: '100%',
-              maxWidth: 500,
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-          >
-            <Typography>{t('auth.loginTitle')}:</Typography>
-            <Button to="/login" component={RouterLink} fullWidth variant="contained" sx={{ mt: 3 }}>
-              {t('auth.loginButton')}
-            </Button>
-          </Box>
-        </Box>
-        <Box component="footer" sx={{ mb: 3 }}>
-          <Copyright />
-        </Box>
-      </Grid>
-    </Grid>
+        <Typography>{t('auth.loginTitle')}:</Typography>
+        <Button to="/login" component={RouterLink} fullWidth variant="contained" sx={{ mt: 3 }}>
+          {t('auth.loginButton')}
+        </Button>
+      </Box>
+    </ContainerWithHero>
   );
 };
--- frontend/src/ui-components/layouts/AuthLayout.tsx
+++ frontend/src/ui-components/layouts/AuthLayout.tsx
@@ -1,62 +1,14 @@
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
 import { ReactNode } from 'react';
 
-import heroIcon from '../../assets/icons/hero.svg';
-import { Copyright } from '../Copyright';
-import { Logo } from '../Icons';
+import { ContainerWithHero } from './common/Container';
 
-interface Props {
+interface AuthLayoutProps {
   children: ReactNode;
 }
 
-export const AuthLayout = ({ children }: Props) => (
-  <Grid container component="main" sx={{ minHeight: '100vh' }}>
-    <Grid
-      item
-      xs={0}
-      md={6}
-      sx={{
-        display: { xs: 'none', md: 'block' },
-        backgroundImage: `url(${heroIcon})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '80% 80%',
-        backgroundPosition: 'center',
-      }}
-    />
-    <Grid
-      item
-      component={Paper}
-      elevation={6}
-      square
-      xs={12}
-      md={6}
-      sx={{
-        px: 5,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}
-    >
-      <Box
-        component="section"
-        sx={{
-          width: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexGrow: 1,
-        }}
-      >
-        <Logo />
-        <Box sx={{ width: '100%', mt: 10 }}>{children}</Box>
-      </Box>
-      <Box component="footer" sx={{ mb: 3 }}>
-        <Copyright />
-      </Box>
-    </Grid>
-  </Grid>
+export const AuthLayout = ({ children }: AuthLayoutProps) => (
+  <ContainerWithHero>
+    <Box sx={{ width: '100%', mt: 10 }}>{children}</Box>
+  </ContainerWithHero>
 );
