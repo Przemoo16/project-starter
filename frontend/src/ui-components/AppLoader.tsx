2a5f3ab86be381a8a058f10f5ebf6a668828b10f
Przemysław Kanach <przemyslaw.kanach@stxnext.pl>
2022-04-20 08:44:13 +0200

    Create App Loader

--- frontend/src/ui-components/AppLoader.tsx
+++ frontend/src/ui-components/AppLoader.tsx
@@ -1,1 +1,14 @@
-export const AppLoader = () => <div>Loading...</div>;
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export const AppLoader = () => (
+  <Box
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }}
+  >
+    <CircularProgress />
+  </Box>
+);
