009123bad6b6ba3c44fefaec9cf68b9670fb9edd
Przemek Kanach <kanach16@gmail.com>
2022-06-24 20:25:44 +0200

    Add dashboard tests and improve e2e tree

---
+++ frontend/src/ui-components/layouts/DashboardLayout.test.tsx
@@ -0,0 +1,24 @@
+import userEvent from '@testing-library/user-event';
+
+import { createMatchMedia, render, screen, theme, waitFor } from '../../tests/utils';
+import { DashboardLayout } from './DashboardLayout';
+
+describe('DashboardLayout component', () => {
+  it('toggles the drawer', async () => {
+    window.matchMedia = createMatchMedia(theme.breakpoints.values.md);
+    const user = userEvent.setup();
+    render(<DashboardLayout>Content</DashboardLayout>);
+
+    expect(screen.getByTestId('drawer')).toHaveStyle({ visibility: 'hidden' });
+
+    await user.click(screen.getByLabelText('open menu'));
+
+    await waitFor(() =>
+      expect(screen.getByTestId('drawer')).toHaveStyle({ visibility: 'visible' })
+    );
+
+    await user.click(screen.getByTestId('navbar'));
+
+    await waitFor(() => expect(screen.getByTestId('drawer')).toHaveStyle({ visibility: 'hidden' }));
+  });
+});
