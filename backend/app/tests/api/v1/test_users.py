1142a0110707945e7ebbd7add7d2a50e46eb9ce6
Przemek Kanach <kanach16@gmail.com>
2022-08-02 08:38:17 +0200

    Make user api tests more unit

--- backend/app/tests/api/v1/test_users.py
+++ backend/app/tests/api/v1/test_users.py
@@ -1,10 +1,11 @@
 import typing
+from unittest import mock
 
 from fastapi import status
 import fastapi_jwt_auth as jwt_auth
 import pytest
 
-from app.tests.helpers import reset_password as reset_password_helpers
+from app.exceptions.http import user as user_exceptions
 from app.tests.helpers import response as response_helpers
 from app.tests.helpers import user as user_helpers
 
@@ -15,9 +16,18 @@ API_URL = "/api/v1"
 
 
 @pytest.mark.anyio
-async def test_create_user(async_client: "conftest.TestClient") -> None:
+@mock.patch("app.services.user.UserService.create_user")
+async def test_create_user(
+    mock_create_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
+) -> None:
     email = "test@email.com"
     name = "Test User"
+    user = await user_helpers.create_active_user(
+        session=session, email=email, name=name
+    )
+    mock_create_user.return_value = user
     request_data = {"email": email, "password": "plain_password", "name": name}
 
     response = await async_client.post(
@@ -28,7 +38,7 @@ async def test_create_user(async_client: "conftest.TestClient") -> None:
     assert response.status_code == status.HTTP_201_CREATED
     assert retrieved_user == response_helpers.format_response(
         {
-            "id": retrieved_user["id"],
+            "id": str(user.id),
             "email": email,
             "name": name,
         }
@@ -70,12 +80,15 @@ async def test_get_me_inactive_user(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.update_user")
 async def test_update_me(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    mock_update_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_active_user(session=session, name="Test User")
-    new_name = "Updated Name"
-    request_data = {"name": new_name}
+    mock_update_user.return_value = user
+    request_data = {"name": "Updated Name"}
     token = jwt_auth.AuthJWT().create_access_token(str(user.id))
     headers = {"Authorization": f"Bearer {token}"}
 
@@ -89,7 +102,7 @@ async def test_update_me(
         {
             "id": user.id,
             "email": user.email,
-            "name": new_name,
+            "name": user.name,
         }
     )
 
@@ -111,8 +124,11 @@ async def test_update_me_inactive_user(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.delete_user", return_value=None)
 async def test_delete_me(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    _: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_active_user(session=session)
     token = jwt_auth.AuthJWT().create_access_token(str(user.id), fresh=True)
@@ -151,8 +167,11 @@ async def test_delete_me_no_fresh_token(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.change_password", return_value=None)
 async def test_change_my_password(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    _: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_active_user(
         session=session,
@@ -190,10 +209,14 @@ async def test_change_my_password_inactive_user(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.get_user")
 async def test_get_user(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    mock_get_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_active_user(session=session)
+    mock_get_user.return_value = user
     token = jwt_auth.AuthJWT().create_access_token(str(user.id))
     headers = {"Authorization": f"Bearer {token}"}
 
@@ -224,10 +247,16 @@ async def test_get_user_inactive_user(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.get_user")
+@mock.patch("app.services.user.UserService.confirm_email", return_value=None)
 async def test_confirm_email(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    _: mock.AsyncMock,
+    mock_get_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_user(session=session)
+    mock_get_user.return_value = user
     request_data = {"token": str(user.email_confirmation_token)}
 
     response = await async_client.post(
@@ -239,10 +268,16 @@ async def test_confirm_email(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.get_active_user")
+@mock.patch("app.services.user.UserService.reset_password", return_value=None)
 async def test_reset_password(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    _: mock.AsyncMock,
+    mock_get_active_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
+    session: "conftest.AsyncSession",
 ) -> None:
     user = await user_helpers.create_active_user(session=session)
+    mock_get_active_user.return_value = user
     request_data = {"email": user.email}
 
     response = await async_client.post(
@@ -261,7 +296,14 @@ async def test_reset_password(
 
 
 @pytest.mark.anyio
-async def test_reset_password_user_not_found(
+@mock.patch(
+    "app.services.user.UserService.get_active_user",
+    side_effect=user_exceptions.UserNotFoundError,
+)
+@mock.patch("app.services.user.UserService.reset_password", return_value=None)
+async def test_reset_password_user_not_found(  # pylint: disable=unused-argument
+    mock_reset_password: mock.AsyncMock,
+    mock_get_active_user: mock.AsyncMock,
     async_client: "conftest.TestClient",
 ) -> None:
     request_data = {"email": "test@email.com"}
@@ -282,11 +324,17 @@ async def test_reset_password_user_not_found(
 
 
 @pytest.mark.anyio
-async def test_reset_password_inactive_user(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+@mock.patch(
+    "app.services.user.UserService.get_active_user",
+    side_effect=user_exceptions.InactiveUserError,
+)
+@mock.patch("app.services.user.UserService.reset_password", return_value=None)
+async def test_reset_password_inactive_user(  # pylint: disable=unused-argument
+    mock_reset_password: mock.AsyncMock,
+    mock_get_active_user: mock.AsyncMock,
+    async_client: "conftest.TestClient",
 ) -> None:
-    user = await user_helpers.create_user(session=session)
-    request_data = {"email": user.email}
+    request_data = {"email": "test@email.com"}
 
     response = await async_client.post(
         f"{API_URL}/users/password/reset", json=request_data
@@ -304,16 +352,15 @@ async def test_reset_password_inactive_user(
 
 
 @pytest.mark.anyio
+@mock.patch("app.services.user.UserService.set_password", return_value=None)
 async def test_set_password(
-    async_client: "conftest.TestClient", session: "conftest.AsyncSession"
+    _: mock.AsyncMock,
+    async_client: "conftest.TestClient",
 ) -> None:
-    user = await user_helpers.create_active_user(session=session)
-    token = await reset_password_helpers.create_reset_password_token(
-        session, user_id=user.id
-    )
+    token = "1dd53909-fcda-4c72-afcd-1bf4886389f8"
 
     request_data = {
-        "token": str(token.id),
+        "token": token,
         "password": "plain_password",
     }
 
